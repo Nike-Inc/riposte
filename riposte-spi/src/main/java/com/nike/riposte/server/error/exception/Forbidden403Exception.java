efe65eb3c180642013c6a9ce6c0e3f1826554de8
Paul Evans <paul.evans@nike.com>
2016-11-22 15:35:54 -0800

    Add support for 403 responses (#7)
    * Add support for 403 responses

---
+++ riposte-spi/src/main/java/com/nike/riposte/server/error/exception/Forbidden403Exception.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.nike.riposte.server.error.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.nike.internal.util.Pair;
+
+/**
+ * Thrown when a request does not have a valid authorization header. Represents a HTTP 403 response code.
+ * 
+ * @author pevans
+ *
+ */
+public class Forbidden403Exception extends RuntimeException {
+
+    public final String requestPath;
+    public final String authorizationHeader;
+    public final List<Pair<String, String>> extraDetailsForLogging;
+    
+    public Forbidden403Exception(String message, String requestPath, String authorizationHeader) {
+        this(message, requestPath, authorizationHeader, new ArrayList<>());
+    }
+
+    public Forbidden403Exception(String message, String requestPath, String authorizationHeader,
+                                    List<Pair<String, String>> extraDetailsForLogging) {
+        super(message);
+
+        this.requestPath = requestPath;
+        this.authorizationHeader = authorizationHeader;
+        this.extraDetailsForLogging = extraDetailsForLogging;
+    }
+    private static final long serialVersionUID = 4921880566299500314L;
+
+}
---
+++ riposte-spi/src/test/java/com/nike/riposte/server/error/exception/Forbidden403ExceptionTest.java
@@ -0,0 +1,32 @@
+package com.nike.riposte.server.error.exception;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Forbidden403ExceptionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void should_honor_constructor_params() {
+        //given
+        String requestPath = UUID.randomUUID().toString();
+        String authorizationHeader = UUID.randomUUID().toString();
+        String message = UUID.randomUUID().toString();
+
+        //when
+        Forbidden403Exception ex = new Forbidden403Exception(message, requestPath, authorizationHeader);
+
+        //then
+        assertThat(ex.getMessage(), is(message));
+        assertThat(ex.requestPath, is(requestPath));
+        assertThat(ex.authorizationHeader, is(authorizationHeader));
+    }
+
+}
