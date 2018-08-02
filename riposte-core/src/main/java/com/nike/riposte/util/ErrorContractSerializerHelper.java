c5161af5d3c8013dfe589c480e9e8580cefed270
Nathanial Myers <Nathanial.Myers@nike.com>
2018-08-02 12:57:59 -0700

    fixes #107

--- riposte-core/src/main/java/com/nike/riposte/util/ErrorContractSerializerHelper.java
+++ riposte-core/src/main/java/com/nike/riposte/util/ErrorContractSerializerHelper.java
@@ -35,12 +35,17 @@ public class ErrorContractSerializerHelper {
     public static ErrorResponseBodySerializer asErrorResponseBodySerializer(ObjectMapper objectMapper) {
         return errorResponseBody -> {
             try {
-                if (errorResponseBody == null || errorResponseBody.bodyToSerialize() == null) {
+                Object bodyToSerialize = (errorResponseBody == null) ?  null : errorResponseBody.bodyToSerialize();
+                if (bodyToSerialize == null) {
                     // errorResponseBody itself is null, or errorResponseBody.bodyToSerialize() is null. Either case
                     //      indicates empty response body payload, so we should return null.
                     return null;
                 }
-                return objectMapper.writeValueAsString(errorResponseBody.bodyToSerialize());
+                if(bodyToSerialize instanceof CharSequence) {
+                    return bodyToSerialize.toString();
+                } else {
+                    return objectMapper.writeValueAsString(bodyToSerialize);
+                }
             }
             catch (JsonProcessingException e) {
                 throw new RuntimeException("An error occurred while serializing an ErrorResponseBody to a string", e);
